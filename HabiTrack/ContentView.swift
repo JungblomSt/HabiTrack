6d4956c7d92956c5e6e9f33cf0b82e8fd73c3288
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2026-04-28 12:53:46 +0200

    Initial Commit

---
+++ HabiTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabiTrack
+//
+//  Created by Stina Thun on 2026-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabiTrack/HabiTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiTrackApp.swift
+//  HabiTrack
+//
+//  Created by Stina Thun on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct HabiTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
