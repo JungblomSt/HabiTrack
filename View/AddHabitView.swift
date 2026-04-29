a91819a6c95eba9dcac3184f1216b918fe7007aa
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2026-04-29 12:37:41 +0200

    setup AddHabitView UI

--- View/AddHabitView.swift
+++ View/AddHabitView.swift
@@ -3,7 +3,30 @@ import SwiftUI
 
 struct AddHabitView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Add Habit")
+                .font(Font.largeTitle)
+                .bold()
+                .padding(10)
+            TextField("New Habit", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button {
+
+            } label: {
+                HStack {
+                    Image(systemName: "plus.circle.fill")
+                    Text("Start New Habit")
+                }
+                .foregroundStyle(Color(.white))
+                .font(.title2)
+                .frame(maxWidth: .infinity)
+                .padding(20)
+                .background(Color(.green))
+                .cornerRadius(20)
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
